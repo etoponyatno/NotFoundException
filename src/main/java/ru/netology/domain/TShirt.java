3704e624d77de5d67a9ceba1fa013c56c88c5604
Petr Filatov <pmfilatov@gmail.com>
2020-10-30 15:55:47 +0300

    added exception test

--- src/main/java/ru/netology/domain/TShirt.java
+++ src/main/java/ru/netology/domain/TShirt.java
@@ -6,6 +6,33 @@ public class TShirt extends Product {
   private String color;
   private String size;
 
+  public TShirt(String color, String size) {
+    this.color = color;
+    this.size = size;
+  }
+
+  public TShirt(int id, String name, int price, String color, String size) {
+    super(id, name, price);
+    this.color = color;
+    this.size = size;
+  }
+
+  public String getColor() {
+    return color;
+  }
+
+  public void setColor(String color) {
+    this.color = color;
+  }
+
+  public String getSize() {
+    return size;
+  }
+
+  public void setSize(String size) {
+    this.size = size;
+  }
+
   @Override
   public boolean equals(Object o) {
     if (this == o) return true;
